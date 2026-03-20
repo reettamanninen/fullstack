1c718cb225b6c5df165cfd7853edd706a6970b1b
reettamanninen <retu.manninen@gmail.com>
2026-03-20 19:47:18 +0200

    1.10

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from 'react'
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const StatisticLine = ({text, value}) => (
+  <p>{text} {value}</p>
+)
 
 const Statistics = ({good, neutral, bad}) => {
   const all = good + neutral + bad
@@ -6,15 +10,17 @@ const Statistics = ({good, neutral, bad}) => {
   if (all === 0) {
     return <p>No feedback given</p>
   }
+
+  
  
  return (
   <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {all === 0 ? 0:(good - bad) / all}</p>
-      <p>positive {all === 0 ? 0:(good / all)*100} %</p>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={(good-bad)/all} />
+      <StatisticLine text="positive" value={(good/all)*100 + "%"} />
   </div>
 )
 }
@@ -24,15 +30,16 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  
+  
 
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button onClick={() => setGood(good + 1)}text = "good" />
+      <Button onClick={() => setNeutral(neutral + 1)}text = "neutral" />
+      <Button onClick={() => setBad(bad + 1)}text = "bad" />
       <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
