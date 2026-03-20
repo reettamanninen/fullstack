3983a88eb23e1c17c022c7b801a8e821f5b6531b
reettamanninen <retu.manninen@gmail.com>
2026-03-20 17:36:27 +0200

    1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+const all = good + neutral + bad;
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -16,6 +18,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {all === 0 ? 0:(good - bad) / all}</p>
+      <p>positive {all === 0 ? 0:(good / all)*100} %</p>
     </div>
   )
 }
