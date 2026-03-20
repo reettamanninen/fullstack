37640813e5e77e744e9b117f90a2d8c4bbefa8cb
reettamanninen <retu.manninen@gmail.com>
2026-03-20 19:54:59 +0200

    1.11

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 const StatisticLine = ({text, value}) => (
-  <p>{text} {value}</p>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 )
 
 const Statistics = ({good, neutral, bad}) => {
@@ -35,14 +38,14 @@ const App = () => {
 
 
   return (
-    <div>
+    <table>
       <h1>give feedback</h1>
       <Button onClick={() => setGood(good + 1)}text = "good" />
       <Button onClick={() => setNeutral(neutral + 1)}text = "neutral" />
       <Button onClick={() => setBad(bad + 1)}text = "bad" />
       <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
-    </div>
+    </table>
   )
 }
 
