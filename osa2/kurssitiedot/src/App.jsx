65b62bfcfda8ac1ec2c22e8b82bb3a2659832083
reettamanninen <retu.manninen@gmail.com>
2026-03-25 19:16:16 +0200

    2.1

---
+++ osa2/kurssitiedot/src/App.jsx
@@ -0,0 +1,55 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+
+  const Course = ({course}) => {
+    const Header = ({course}) => 
+      <h1>{course}</h1>
+   
+
+    const Content = ({parts}) => (
+    <div>
+      {parts.map(part => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+   )
+    const Part = ({part}) => (
+      <div>{part.name} {part.exercises}</div>
+    )
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+    </div>
+  )
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+  }
+export default App
