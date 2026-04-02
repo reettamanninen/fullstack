f11bcb38dc63a02fb705749ad98b0074d266b8d5
reettamanninen <retu.manninen@gmail.com>
2026-04-02 21:13:16 +0300

    3.9

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const { request, response } = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
+
+app.use(cors())
+
 
 app.use(express.json())
 
