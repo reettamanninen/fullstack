406fc4d8e3dcf45b24f069da9b2ebecd91d82ec5
reettamanninen <retu.manninen@gmail.com>
2026-04-02 21:40:08 +0300

    muokattu portti

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -105,6 +105,7 @@ app.delete('/api/persons/:id', (request, response) => {
   })
 
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
