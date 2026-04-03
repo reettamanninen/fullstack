396f4c6119253ad243421107cb813fe22ab6143a
reettamanninen <retu.manninen@gmail.com>
2026-04-03 11:51:41 +0300

    3.14 poisto ei toimi

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -22,30 +22,6 @@ app.use(morgan((tokens, request, response) => {
 }))
 
 
-
-let persons =[
-    {
-        id: 1,
-        name: 'Arto Hellas',
-        number: '040-123456'
-    },
-    {
-        id: 2,
-        name: 'Ada Lovelace',
-        number: '39-44-5323523'
-      },
-      {
-        id: 3,
-        name: 'Dan Abramov',
-        number: '12-43-234345'
-      },
-      {
-        id: 4,
-        name: 'Mary Poppendieck',
-        number: '39-23-6423122'
-      }
-]
-
 app.get('/info', (request, response) => {
     const count = persons.length
     const time = new Date()
@@ -60,52 +36,38 @@ Person.find({}).then(persons => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-        const person = persons.find(person => person.id === id)
-
-        if (person) {
-            response.json(person)
-          } else {
-            response.status(404).end('error: person not found')
-          }
+    Person.findById(request.params.id)
+      .then(person => {
+        response.json(person)
+      }
+    )
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
+  Person.findById(request.params.id)
+    .then(() => {
     response.status(204).end()
   })
+  })
+
+  
 
   app.post('/api/persons', (request, response) => {
-    const body =request.body
-    if (!body.name) {
-        return response.status(400).json({ 
-          error: 'name missing' })
-        }
-    if (!body.number) {
-        return response.status(400).json({ 
-        error: 'number missing' })
-        }
-
-    const id = Math.floor(Math.random() * 10000)
-
-    const person = {
-        id: id,
-        name: body.name,
-        number: body.number
+    const body = request.body
+  
+    if (!body.name || !body.number) {
+      return response.status(400).json({ error: 'name or number missing' })
     }
+  
+    const person = new Person ({
+      name: body.name,
+      number: body.number
+  })
 
-    if (persons.some(person => person.name === body.name)) {
-        return response.status(400).json({ 
-            error: 'name must be unique' })
-    }
-
-    persons = persons.concat(person)
-    response.json(person)
-
-        
-    
+  
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
   })
 
 
