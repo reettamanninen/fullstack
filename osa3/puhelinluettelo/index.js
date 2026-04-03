10a91c08e3451c16bdd170908361fb630afc0433
reettamanninen <retu.manninen@gmail.com>
2026-04-03 12:31:25 +0300

    3.16

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -22,37 +22,43 @@ app.use(morgan((tokens, request, response) => {
 }))
 
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
     const count = persons.length
     const time = new Date()
     response.send(`<p> Phonebook has info for ${count} people </p>
     <p> ${time} </p>`)}
 )
 
-app.get('/api/persons',(request, response) => {
+app.get('/api/persons',(request, response, next) => {
 Person.find({}).then(persons => {
   response.json(persons)
   }) 
+  .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
       .then(person => {
-        response.json(person)
-      }
-    )
+        if (person) {
+          response.json(person)
+        } else {
+          response.status(404).end()
+        }
+      })
+      .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(() => {
     response.status(204).end()
   })
+  .catch(error => next(error))
   })
 
   
 
-  app.post('/api/persons', (request, response) => {
+  app.post('/api/persons', (request, response, next) => {
     const body = request.body
   
     if (!body.name || !body.number) {
@@ -68,8 +74,23 @@ app.delete('/api/persons/:id', (request, response) => {
     person.save().then(savedPerson => {
       response.json(savedPerson)
     })
+    .catch(error => next(error))
   })
 
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    return response.status(500).json({error: 'jottain on pielessä :('})
+  
+    next(error)
+  }
+  
+  
+  app.use(errorHandler)
 
   const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
