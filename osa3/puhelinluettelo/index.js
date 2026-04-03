38d42edbb036eab71e4d2c3c9a7a00df85bd55b9
reettamanninen <retu.manninen@gmail.com>
2026-04-03 12:05:08 +0300

    3.15

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -44,7 +44,7 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id)
+  Person.findByIdAndDelete(request.params.id)
     .then(() => {
     response.status(204).end()
   })
