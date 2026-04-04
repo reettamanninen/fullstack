87b549136af5a5027fec760af022be8c077c997a
reettamanninen <retu.manninen@gmail.com>
2026-04-04 13:03:54 +0300

    3.20

--- osa3/puhelinluettelo/models/person.js
+++ osa3/puhelinluettelo/models/person.js
@@ -21,7 +21,18 @@ const personSchema = new mongoose.Schema({
         minLength: 3,
         required: true
     },
-    number: String
+    number: {
+        type: String,
+        minLength: 8,
+        requied: true,
+        validate: {
+          validator: function(v) {
+            return /\d{2,3}-\d{5}/.test(v);
+          },
+          message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required']
+      }
 })
 
 personSchema.set('toJSON', {
