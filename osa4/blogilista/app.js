dc7f7faf882d1777727716dfbbdd3feea6ccc601
reettamanninen <retu.manninen@gmail.com>
2026-05-11 10:55:06 +0300

    4.18

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -6,6 +6,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogitRouter = require('./controllers/blogit')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -26,6 +27,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogit', blogitRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
