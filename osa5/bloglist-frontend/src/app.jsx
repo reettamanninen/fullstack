29a50c4d93223414374bedee46b21461ed8b5205
reettamanninen <retu.manninen@gmail.com>
2026-05-13 12:14:14 +0300

    5.2

--- osa5/bloglist-frontend/src/app.jsx
+++ osa5/bloglist-frontend/src/app.jsx
@@ -11,15 +11,19 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
   }, [])
 
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
     const user = await loginService.login({ username, password})
+    window.localStorage.setItem('loggedUser', JSON.stringify(user))
     blogService.setToken(user.token)
     setUser(user)
     setUsername('')
@@ -30,6 +34,11 @@ const App = () => {
       setErrorMessage(null), 5000)
     
   }
+}
+
+const handleLogout = () => {
+  window.localStorage.removeItem('loggedUser')
+  setUser(null)
 }
 
 
@@ -66,7 +75,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in </p>
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
