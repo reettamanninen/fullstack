c90f33d75d3bc5b6e1f5500fa1f7468332662e48
reettamanninen <retu.manninen@gmail.com>
2026-05-14 15:07:15 +0300

    5.15

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -39,3 +39,28 @@ const blog = {
   expect(screen.getByText( 2, { exact: false})).toBeDefined()
   expect(screen.getByText('skdjbgkwe', { exact: false})).toBeDefined()
 })
+
+test ('when like is pressed twice funktio is called twice', async () => {
+    const blog = {
+        title: 'sjbdsgke',
+        author: 'ajfbekjgb',
+        url: 'adjbfkwe',
+        likes: 2,
+        user: {username: 'asjnbkwfe', name: 'skdjbgkwe' }
+      }
+    
+      const mockHandler = vi.fn()
+
+      render(<Blog blog={blog} handleLike={mockHandler}
+      user={{ username: 'asjnbkwfe'}} />)
+      
+      const user = userEvent.setup()
+        const viewButton = screen.getByText('view')
+        await user.click(viewButton)
+
+      const likeButton = screen.getByText('like')
+      await user.click(likeButton)
+      await user.click(likeButton)
+      expect(mockHandler).toHaveBeenCalled(2)
+    
+    })
